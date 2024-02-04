e7347823e155f79aac65f08054257f93a4783db1
bayanwafa <bayan.wafa@gmail.com>
2024-02-03 20:27:41 -0800

    change in sequelize file

--- config/Sequelize.js
+++ config/Sequelize.js
@@ -1,9 +1,9 @@
-const {sequelize}=require("sequelize");
+const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASWWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "postgres",
-  });
+});
 
-  module.exports = sequelize;
+module.exports = sequelize;
